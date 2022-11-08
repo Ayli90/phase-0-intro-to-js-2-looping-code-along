f267729916a12300c8d60dd35c877f9826f3b563
Amy Li <amyl@buffalo.edu>
2022-11-07 21:47:45 -0500

    commit tree af3689fc3e917a6da3ab69cf7ff2d80a235e974b

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(names, party) {
+    const messages = []
+    for (let i = 0; i < names.length; i++) {
+        
+    const msg = "Thank you, "+names[i]+", for the wonderful " +party+" gift!";
+    messages.push(msg);
+    }
+    //console.log(messages)
+    return messages;
+}
+
+
+new_msg = writeCards( ["Guadalupe", "Ollie", "Aki"],"birthday");
+console.log(new_msg)
+
+function countDown(countdown) { 
+while (countdown > -1) {
+    console.log(countdown--);
+
+}
+}
